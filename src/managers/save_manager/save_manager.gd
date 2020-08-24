95eddfd988f9c83ccd61c2e2590f55d875cec247
Quentin Daveau <daveau.quentin@hotmail.fr>
2020-08-24 12:40:53 +0200

    Add first start tutorial text

--- src/managers/save_manager/save_manager.gd
+++ src/managers/save_manager/save_manager.gd
@@ -4,24 +4,26 @@ extends Node
 export(NodePath) var _time_manager_path
 
 onready var _time_manager = get_node(_time_manager_path)
+onready var _transit_screen = get_parent().get_node("TransitScreen")
 
 
 func _ready() -> void:
 	var save_game = File.new()
 	if not save_game.file_exists("user://savegame.save"):
 		_time_manager.first_launch()
+		_transit_screen.open(true)
 		return 
 	save_game.open("user://savegame.save", File.READ)
 	_time_manager.load_time(parse_json(save_game.get_line()))
 	save_game.close()
+	_transit_screen.open()
 
 
 func _notification(what):
 	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
 		prepare_and_save()
-		var transit_screen = get_parent().get_node("TransitScreen")
-		transit_screen.close()
-		yield(transit_screen, "closing_done")
+		_transit_screen.close()
+		yield(_transit_screen, "closing_done")
 		get_tree().quit()
 
 
--- src/world/transit_screen.gd
+++ src/world/transit_screen.gd
@@ -2,9 +2,23 @@ extends ColorRect
 
 signal closing_done
 
-func _ready():
-	$Tween.interpolate_property(self, "color:a", 1, 0, 3, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-	$Tween.start()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		_validate_open()
+		set_process(false)
+
+
+func open(first_launch: bool = false):
+	if not first_launch:
+		$Tween.interpolate_property(self, "color:a", 1, 0, 3, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		$Tween.start()
+	else:
+		$Tween.interpolate_property($Label, "custom_colors/font_color:a", 0, 1, 2,\
+				Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		$Tween.start()
+		yield($Tween, "tween_all_completed")
+		set_process(true)
 
 
 func close() -> void:
@@ -13,6 +27,13 @@ func close() -> void:
 	$Tween.start()
 	yield($Tween, "tween_all_completed")
 	emit_signal("closing_done")
+
+
+func _validate_open() -> void:
+	$Tween.interpolate_property(self, "color:a", 1, 0, 3, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Label, "custom_colors/font_color:a", 1, 0, 2,\
+				Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.start()
 
 
 func _set_audio_level(level: float) -> void:
